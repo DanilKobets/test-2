ff622185b9508a181de6af7137a942c7caa20032
Danil Kobets <kobec.danil@gmail.com>
2023-06-10 12:28:31 +0300

    a+b

--- Examples/Examples001/Program.cs
+++ Examples/Examples001/Program.cs
@@ -1,5 +1,4 @@
-﻿Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
-
+﻿int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
